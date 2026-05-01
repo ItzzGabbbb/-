3e87dd8b452ea00f3ad9b017c07edd0374e31dba
ItzzGabbbb <gabriel.gulberti.12@gmail.com>
2026-05-01 21:51:13 +0200

    Add files via upload

---
+++ Dark Mode Fixed.lua
@@ -0,0 +1,25 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Create ClientModules folder if it doesn't exist
+local clientModules = ReplicatedStorage:FindFirstChild("ClientModules")
+if not clientModules then
+    clientModules = Instance.new("Folder")
+    clientModules.Name = "ClientModules"
+    clientModules.Parent = ReplicatedStorage
+end
+
+-- Find ModulesClient folder
+local modulesClient = ReplicatedStorage:FindFirstChild("ModulesClient")
+if modulesClient then
+    local moduleEvents = modulesClient:FindFirstChild("Module_Events")
+    if moduleEvents then
+        moduleEvents.Parent = clientModules
+        print("Successfully moved Module_Events to ClientModules!")
+    else
+        warn("Module_Events not found inside ModulesClient!")
+    end
+else
+    warn("ModulesClient folder not found in ReplicatedStorage!")
+end
+wait(0.1)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Dark%20Mode/source.lua'))()
